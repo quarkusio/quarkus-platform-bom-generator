87a9b8486e7b247858fae3122348d013e5671080
Alexey Loubyansky <olubyans@redhat.com>
2023-08-23 13:18:26 +0200

    Release detectors for Google SDK platform java subprojects

---
+++ bom-decomposer/src/main/java/io/quarkus/bom/decomposer/detector/GoogleCloudIamReleaseIdDetector.java
@@ -0,0 +1,34 @@
+package io.quarkus.bom.decomposer.detector;
+
+import io.quarkus.bom.decomposer.BomDecomposerException;
+import io.quarkus.bom.decomposer.ReleaseId;
+import io.quarkus.bom.decomposer.ReleaseIdDetector;
+import io.quarkus.bom.decomposer.ReleaseIdFactory;
+import io.quarkus.bom.decomposer.ReleaseIdResolver;
+import io.quarkus.bom.decomposer.ReleaseOrigin;
+import io.quarkus.bom.decomposer.ReleaseVersion;
+import org.eclipse.aether.artifact.Artifact;
+
+public class GoogleCloudIamReleaseIdDetector implements ReleaseIdDetector {
+
+    @Override
+    public ReleaseId detectReleaseId(ReleaseIdResolver releaseResolver, Artifact artifact)
+            throws BomDecomposerException {
+        if (artifact.getGroupId().equals("com.google.cloud")
+                && artifact.getArtifactId().contains("google-iam")) {
+            // these artifacts are released from https://github.com/googleapis/sdk-platform-java
+            // along with other artifacts that are versioned differently
+            var releaseId = releaseResolver.defaultReleaseId(artifact);
+            if (releaseId.version().isTag()) {
+                return releaseId;
+            }
+            var origin = releaseId.origin();
+            if (origin.isUrl() && origin.toString().equals("https://github.com/googleapis/sdk-platform-java")) {
+                return ReleaseIdFactory.create(ReleaseOrigin.Factory.ga("com.google.cloud", "google-iam-parent"),
+                        ReleaseVersion.Factory.version(artifact.getVersion()));
+            }
+        }
+        return null;
+    }
+
+}
